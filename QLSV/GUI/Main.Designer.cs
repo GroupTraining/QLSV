0841f1ad61c24a0dd1b549af50675d40ff6ab644
hungzr <36071597+hungzr@users.noreply.github.com>
2018-03-14 10:42:32 +0700

    master

---
+++ QLSV/GUI/Main.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace QLSV.GUI
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.galleryContainer2 = new DevComponents.DotNetBar.GalleryContainer();
+            this.labelItem1 = new DevComponents.DotNetBar.LabelItem();
+            this.contextMenuBar1 = new DevComponents.DotNetBar.ContextMenuBar();
+            this.buttonX1 = new DevComponents.DotNetBar.ButtonX();
+            this.buttonX2 = new DevComponents.DotNetBar.ButtonX();
+            this.buttonX3 = new DevComponents.DotNetBar.ButtonX();
+            this.buttonX4 = new DevComponents.DotNetBar.ButtonX();
+            this.buttonX5 = new DevComponents.DotNetBar.ButtonX();
+            ((System.ComponentModel.ISupportInitialize)(this.contextMenuBar1)).BeginInit();
+            this.contextMenuBar1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // galleryContainer2
+            // 
+            // 
+            // 
+            // 
+            this.galleryContainer2.BackgroundStyle.Class = "RibbonFileMenuColumnTwoContainer";
+            this.galleryContainer2.EnableGalleryPopup = false;
+            this.galleryContainer2.LayoutOrientation = DevComponents.DotNetBar.eOrientation.Vertical;
+            this.galleryContainer2.MinimumSize = new System.Drawing.Size(180, 240);
+            this.galleryContainer2.MultiLine = false;
+            this.galleryContainer2.Name = "galleryContainer2";
+            this.galleryContainer2.PopupUsesStandardScrollbars = false;
+            // 
+            // labelItem1
+            // 
+            this.labelItem1.BorderSide = DevComponents.DotNetBar.eBorderSide.Bottom;
+            this.labelItem1.BorderType = DevComponents.DotNetBar.eBorderType.Etched;
+            this.labelItem1.CanCustomize = false;
+            this.labelItem1.Name = "labelItem1";
+            // 
+            // contextMenuBar1
+            // 
+            this.contextMenuBar1.Controls.Add(this.buttonX5);
+            this.contextMenuBar1.Controls.Add(this.buttonX4);
+            this.contextMenuBar1.Controls.Add(this.buttonX3);
+            this.contextMenuBar1.Controls.Add(this.buttonX2);
+            this.contextMenuBar1.Controls.Add(this.buttonX1);
+            this.contextMenuBar1.Location = new System.Drawing.Point(2, 0);
+            this.contextMenuBar1.Name = "contextMenuBar1";
+            this.contextMenuBar1.Size = new System.Drawing.Size(731, 25);
+            this.contextMenuBar1.Stretch = true;
+            this.contextMenuBar1.Style = DevComponents.DotNetBar.eDotNetBarStyle.Office2003;
+            this.contextMenuBar1.TabIndex = 0;
+            this.contextMenuBar1.TabStop = false;
+            this.contextMenuBar1.Text = "contextMenuBar1";
+            // 
+            // buttonX1
+            // 
+            this.buttonX1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX1.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX1.Location = new System.Drawing.Point(3, 0);
+            this.buttonX1.Name = "buttonX1";
+            this.buttonX1.Size = new System.Drawing.Size(75, 25);
+            this.buttonX1.TabIndex = 0;
+            this.buttonX1.Text = "Home";
+            this.buttonX1.Click += new System.EventHandler(this.buttonX1_Click);
+            // 
+            // buttonX2
+            // 
+            this.buttonX2.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX2.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX2.Location = new System.Drawing.Point(91, 0);
+            this.buttonX2.Name = "buttonX2";
+            this.buttonX2.Size = new System.Drawing.Size(75, 25);
+            this.buttonX2.TabIndex = 1;
+            this.buttonX2.Text = "Thêm mới";
+            this.buttonX2.Click += new System.EventHandler(this.buttonX2_Click);
+            // 
+            // buttonX3
+            // 
+            this.buttonX3.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX3.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX3.Location = new System.Drawing.Point(179, 0);
+            this.buttonX3.Name = "buttonX3";
+            this.buttonX3.Size = new System.Drawing.Size(75, 25);
+            this.buttonX3.TabIndex = 2;
+            this.buttonX3.Text = "Tra cứu";
+            this.buttonX3.Click += new System.EventHandler(this.buttonX3_Click);
+            // 
+            // buttonX4
+            // 
+            this.buttonX4.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX4.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX4.Location = new System.Drawing.Point(267, 0);
+            this.buttonX4.Name = "buttonX4";
+            this.buttonX4.Size = new System.Drawing.Size(75, 25);
+            this.buttonX4.TabIndex = 3;
+            this.buttonX4.Text = "Chỉnh sửa";
+            this.buttonX4.Click += new System.EventHandler(this.buttonX4_Click);
+            // 
+            // buttonX5
+            // 
+            this.buttonX5.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX5.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX5.Location = new System.Drawing.Point(355, 0);
+            this.buttonX5.Name = "buttonX5";
+            this.buttonX5.Size = new System.Drawing.Size(75, 25);
+            this.buttonX5.TabIndex = 4;
+            this.buttonX5.Text = "Thống kê";
+            this.buttonX5.Click += new System.EventHandler(this.buttonX5_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 312);
+            this.Controls.Add(this.contextMenuBar1);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.contextMenuBar1)).EndInit();
+            this.contextMenuBar1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.GalleryContainer galleryContainer2;
+        private DevComponents.DotNetBar.LabelItem labelItem1;
+        private DevComponents.DotNetBar.ContextMenuBar contextMenuBar1;
+        private DevComponents.DotNetBar.ButtonX buttonX5;
+        private DevComponents.DotNetBar.ButtonX buttonX4;
+        private DevComponents.DotNetBar.ButtonX buttonX3;
+        private DevComponents.DotNetBar.ButtonX buttonX2;
+        private DevComponents.DotNetBar.ButtonX buttonX1;
+    }
+}
---
+++ QLSV/GUI/Main.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLSV.GUI
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            contextMenuBar1.Visible = true;
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonX1_Click(object sender, EventArgs e)//Home
+        {
+
+        }
+
+        private void buttonX2_Click(object sender, EventArgs e)//Thêm
+        {
+
+        }
+
+        private void buttonX3_Click(object sender, EventArgs e)//Tra cứu
+        {
+
+        }
+
+        private void buttonX4_Click(object sender, EventArgs e)//CHỉnh sửa
+        {
+
+        }
+
+        private void buttonX5_Click(object sender, EventArgs e)//Thống kê
+        {
+
+        }
+    }
+}
